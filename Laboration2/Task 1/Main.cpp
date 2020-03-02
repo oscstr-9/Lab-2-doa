e0e1812be6e9ac43442cbf626a6e510d7fd40d25
Oscar Östryd <oscstr-9@student.ltu.se>
2020-03-02 16:37:17 +0100

    Commented some code
    added explanations to funtions

--- Laboration2/Task 1/Main.cpp
+++ Laboration2/Task 1/Main.cpp
@@ -5,6 +5,7 @@
 
 using namespace std;
 
+// traverses through array comparing every number to the previous one to if it needs to swap
 void InsertionSort(int list[], int size) {
 	int temp;
 	int i = 1, counter = 1;
@@ -23,21 +24,22 @@ void InsertionSort(int list[], int size) {
 	}
 }
 
-int binarySearch(int list[], int item, int low, int high){
+// halving array until position for selected vairable is found. takes int array, int that is looking for position, start of array, end of where to look.
+int binarySearch(int list[], int selected, int low, int high){
 
 	if (high <= low)
-		return (item > list[low]) ? (low + 1) : low;
+		return (selected > list[low]) ? (low + 1) : low;
 
 	int mid = (low + high) / 2;
 
-	if (item == list[mid])
+	if (selected == list[mid])
 		return mid + 1;
 
-	if (item > list[mid])
-		return binarySearch(list, item, mid + 1, high);
-	return binarySearch(list, item, low, mid - 1);
+	if (selected > list[mid])
+		return binarySearch(list, selected, mid + 1, high);
+	return binarySearch(list, selected, low, mid - 1);
 }
-
+// moves int to position found by binary search function.
 void bInsertionSort(int list[], int size) {
 	for (int i = 1; i < size; ++i)
 	{
@@ -56,7 +58,7 @@ void bInsertionSort(int list[], int size) {
 	}
 }
 
-
+// takes array of ints, a value for how big chunks to send to sorter, total size of array and bool to choose which sorter to use
 void mergeSort(int* list, int k, int size, bool sorter) {
 	int leftIndex = 0, rightIndex = size / 2;
 	if (size > k) {
@@ -105,7 +107,7 @@ void mergeSort(int* list, int k, int size, bool sorter) {
 void main() {
 	srand(time(0));
 	int sizeOfList = 1000000;
-	int k = 50;
+	int k = 50; // size of chunk to be sent to sorting method
 	bool sorter = true;
 
 	int* list = new int[sizeOfList];
